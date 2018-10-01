c8d9e0ff8943605a563ff79ee2e04e42c2a136fe
Mateus Rovari <mateus@liveonsolutions.com>
2018-10-01 17:19:29 -0300

    backing to home

--- Android-App/app/src/main/java/com/foodproject/activity/ProductActivity.java
+++ Android-App/app/src/main/java/com/foodproject/activity/ProductActivity.java
@@ -1,9 +1,13 @@
 package com.foodproject.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.foodproject.R;
 import com.foodproject.adapter.CategoryAdapter;
@@ -12,6 +16,7 @@ import com.foodproject.adapter.ProductAdapter;
 public class ProductActivity extends AppCompatActivity {
 
     RecyclerView mProductRecycler;
+    private ImageButton mBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,7 @@ public class ProductActivity extends AppCompatActivity {
 
     private void initComponents() {
         mProductRecycler = findViewById(R.id.product_recycler_view);
+        mBack = findViewById(R.id.back_to_home);
     }
 
     private void setupWidgets() {
@@ -34,6 +40,14 @@ public class ProductActivity extends AppCompatActivity {
         mProductRecycler.setLayoutManager(llmProduct);
         ProductAdapter mProductAdapter = new ProductAdapter(this);
         mProductRecycler.setAdapter(mProductAdapter);
+
+        //backing to home
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
 
