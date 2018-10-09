fe097721c18ddceabef74754a1cad27fbd2502f3
Mateus Rovari <mateus@liveonsolutions.com>
2018-10-09 18:12:59 -0300

    setup drawer navigation

--- Android-App/app/src/main/java/com/foodproject/activity/HomeActivity.java
+++ Android-App/app/src/main/java/com/foodproject/activity/HomeActivity.java
@@ -1,12 +1,21 @@
 package com.foodproject.activity;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.foodproject.R;
 import com.foodproject.adapter.CategoryAdapter;
@@ -16,21 +25,24 @@ public class HomeActivity extends AppCompatActivity {
 
     RecyclerView mCategoryRecycler, mProductRecycler;
     private Button mProduct;
+    private ImageButton mMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_menu_drawer_news);
 
 
         initComponents();
         setupWidgets();
+        initNavigationMenu();
     }
 
     private void initComponents() {
         mCategoryRecycler = findViewById(R.id.category_recycler_view);
         mProductRecycler = findViewById(R.id.product_recycler_view);
         mProduct = findViewById(R.id.go_to_products);
+        mMenuButton = findViewById(R.id.bt_menu);
     }
 
     private void setupWidgets() {
@@ -53,6 +65,29 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(HomeActivity.this, RestaurantActivity.class);
                 startActivity(i);
+            }
+        });
+    }
+
+    private void initNavigationMenu() {
+        NavigationView nav_view = findViewById(R.id.nav_view);
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        //on menu item clicked
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(final MenuItem item) {
+                Toast.makeText(getApplicationContext(), item.getTitle() + " Selected", Toast.LENGTH_SHORT).show();
+//                actionBar.setTitle(item.getTitle());
+                drawer.closeDrawers();
+                return true;
+            }
+        });
+
+        mMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(GravityCompat.START);
             }
         });
     }
