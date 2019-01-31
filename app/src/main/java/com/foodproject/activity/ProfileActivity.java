910d986d465724d9c96efa6178ea6dc03067322d
Mateus Rovari <mateus@liveonsolutions.com>
2019-01-31 17:23:23 -0200

    place product card layout

--- app/src/main/java/com/foodproject/activity/ProfileActivity.java
+++ app/src/main/java/com/foodproject/activity/ProfileActivity.java
@@ -4,20 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.foodproject.R;
 import com.foodproject.Utils.BottomNavigationViewHelper;
-import com.foodproject.adapter.TrendingProductAdapter;
-import com.foodproject.model.Products;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -55,7 +49,7 @@ public class ProfileActivity extends AppCompatActivity {
         favoriteFoods.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(ProfileActivity.this, FavoriteFoodsActvity.class);
+                Intent i = new Intent(ProfileActivity.this, FavoriteFoodsActivity.class);
                 startActivity(i);
             }
         });
