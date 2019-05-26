98d2bb69d86e4187f170c0e0b90ead1fe4e9012d
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-05-25 23:36:49 -0300

    fixing favorite foods activity and deleting unused files

---
+++ app/src/main/java/com/foodproject/adapter/FavoriteProductsAdapter.java
@@ -0,0 +1,108 @@
+package com.foodproject.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.foodproject.R;
+import com.foodproject.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavoriteProductsAdapter extends RecyclerView.Adapter<FavoriteProductsAdapter.ItemHolder>{
+
+    private List<Product> products = new ArrayList<>();
+    private Context context;
+    private final OnProductClickListener mListener;
+
+    public FavoriteProductsAdapter(Context context){
+        this.context = context;
+
+        try {
+            this.mListener = ((OnProductClickListener) context);
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Activity must implement OnPlaceClickListener.");
+        }
+
+        for (int i = 0; i < 30; i++){
+            Product prod = new Product("Product " + (i + 1), "Description " + (i + 1),
+                     "Restaurant " + (i + 1), "Product Value R$" + (i + 1) + ",00");
+            products.add(prod);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.product_trending_card, viewGroup, false);
+        ItemHolder holder = new ItemHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ItemHolder holder, int position) {
+        final Product prod =  products.get(position);
+
+        holder.mProductName.setText(prod.getmProductName());
+        holder.mProductDescription.setText(prod.getmProductDescription());
+
+        holder.mNoFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener  != null){
+                    mListener.OnProductFavoriteClick(prod);
+                    holder.mNoFavorite.setVisibility(View.GONE);
+                    holder.mFavorite.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        holder.mFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListener  != null){
+                    mListener.OnProductNoFavoriteClick(prod);
+                    holder.mNoFavorite.setVisibility(View.VISIBLE);
+                    holder.mFavorite.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    public class ItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView mProductName, mProductDescription, mProductValue, mProductRestaurant;
+        public ImageView mNoFavorite, mFavorite;
+
+
+        public ItemHolder(@NonNull View itemView) {
+            super(itemView);
+            mProductName = itemView.findViewById(R.id.product_name);
+            mProductDescription = itemView.findViewById(R.id.product_description);
+            mFavorite = itemView.findViewById(R.id.favorite);
+            mNoFavorite = itemView.findViewById(R.id.no_favorite);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(context, "Clicked Item", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public interface OnProductClickListener {
+        void OnProductNoFavoriteClick(Product products);
+        void OnProductFavoriteClick(Product products);
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+}
