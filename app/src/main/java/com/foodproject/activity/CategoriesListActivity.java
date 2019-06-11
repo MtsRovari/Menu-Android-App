f5a2f3c605a22dcf58176bb28fbe2892da9cd62c
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-06-10 21:34:47 -0300

    categories list activity and rename classes

---
+++ app/src/main/java/com/foodproject/activity/CategoriesListActivity.java
@@ -0,0 +1,70 @@
+package com.foodproject.activity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.foodproject.R;
+import com.foodproject.adapter.CategoriesAdapter;
+import com.foodproject.adapter.PlaceAdapter;
+import com.foodproject.model.Category;
+import com.foodproject.model.Place;
+
+public class CategoriesListActivity extends AppCompatActivity implements CategoriesAdapter.OnCategoryClickListener{
+
+    private RelativeLayout mBack;
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories_list);
+
+        initialize();
+        setupWidgets();
+    }
+
+    private void initialize() {
+        mBack = findViewById(R.id.back);
+        mRecyclerView = findViewById(R.id.recycler_view);
+    }
+
+    private void setupWidgets() {
+        //change status bar color to transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.headerColor));
+        }
+
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        //setup product recycler view
+        GridLayoutManager lnlGrid = new GridLayoutManager(this, 3);
+        mRecyclerView.setLayoutManager(lnlGrid);
+        CategoriesAdapter mPlaceAdapter = new CategoriesAdapter(this);
+        mRecyclerView.setAdapter(mPlaceAdapter);
+    }
+
+    @Override
+    public void onCategoryClickListener(Category category) {
+        Toast.makeText(this, category.getCategoryName(), Toast.LENGTH_SHORT).show();
+    }
+}
